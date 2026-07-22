ed4a165e48c3668f15db7d2b5fc8a98a67995154
Umang-1103 <umangkoladiya123@gmail.com>
2026-07-22 16:36:55 +0530

    R

--- Assignment/React - Fundamentals/Session 10/usecontext/src/App.js
+++ Assignment/React - Fundamentals/Session 10/usecontext/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import UserContext from "./UserContext";
+import ThemeContext from "./ThemeContext";
+import NotificationContext from "./NotificationContext";
 
 function App() {
   return (
     <div className="App">
-      
+      <UserContext/>
+      <ThemeContext/>
+      <NotificationContext/>
     </div>
   );
 }
